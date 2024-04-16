b3cd7a270ed57ce9e1b7a42b8fb10e857cfc5606
John Hughes <johnniallhughes@gmail.com>
2024-04-17 00:58:03 +0100

    added textures and scaling for all planets

--- Python/net-force.py
+++ Python/net-force.py
@@ -10,30 +10,33 @@ class Planet:
     position = [0, 0, 0]
     velocity = [0, 0, 0]
 
-    def __init__(self, name, mass, position, velocity):
+    def __init__(self, name, mass, position, velocity, radius=142984000/2, texture='Venus_2k.png'):
         self.name = name
         self.mass = mass
         self.position = np.array(position)
         self.velocity = np.array(velocity) * 1.1
         self.path = []
+        self.texture = texture
+        self.radius = radius
 
     def print_params(self):
         print(self.name, self.mass, self.position, self.velocity)
 
 
 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/
-v_mw = 720000 * 60 * 60 # m/s
-jupiter = Planet("Jupiter", 1.898e27, [778.5e13, 0, 0], [0, 13.1e6, v_mw])
-saturn  = Planet("Saturn", 5.683e+26, [0.0, 1432.0e13, 0], [-9.7e6, 0, v_mw])
-sun     = Planet("Sun", 1.989e30, [0.0, 0, 0], [0.0, 0, v_mw])
-uranus  = Planet("Uranus", 86.8e24, [2867.0e13, 0.0, 0.0], [0.0, 6.8e6, v_mw])
-neptune = Planet("Neptune", 102e24, [4515.0e13, 0.0, 0.0], [0.0, 64.7e5 / 1.1, v_mw])
+v_mw = 0 #720000# * 60 * 60 # m/s
+jupiter = Planet("Jupiter", 1.898e27, [778.5e13, 0, 0], [0, 13.1e6, v_mw], radius=1.42984e8/2, texture='Jupiter_2k.png')
+saturn  = Planet("Saturn", 5.683e+26, [0.0, 1432.0e13, 0], [-9.7e6, 0, v_mw], radius=1.20536e8/2, texture='Saturn_2k.png')
+sun     = Planet("Sun", 1.989e30, [0.0, 0, 0], [0.0, 0, v_mw], radius=6.96e8, texture='Venus_2k.png')
+uranus  = Planet("Uranus", 86.8e24, [2867.0e13, 0.0, 0.0], [0.0, 6.8e6, v_mw], radius=5.1e7/2, texture='Uranus_2k.png')
+neptune = Planet("Neptune", 102e24, [4515.0e13, 0.0, 0.0], [0.0, 64.7e5 / 1.1, v_mw], radius=4.95e7/2, texture='Neptune_2k.png')
+
 planets = [
     jupiter,
     saturn,
-    sun,
     uranus,
-    neptune
+    neptune,
+    sun
 ]
 
 
@@ -55,11 +58,39 @@ def normalize(v):
     return v/mag
 
 
+def place_planet(R, image, plot, position=[0,0,0], count=20):
+    img = plt.imread(image)
+
+    # define a grid matching the map size, subsample along with pixels
+    theta = np.linspace(0, np.pi, img.shape[0])
+    phi = np.linspace(0, 2*np.pi, img.shape[1])
+
+    theta_inds = np.linspace(0, img.shape[0] - 1, count).round().astype(int)
+    phi_inds = np.linspace(0, img.shape[1] - 1, count).round().astype(int)
+    theta = theta[theta_inds]
+    phi = phi[phi_inds]
+    img = img[np.ix_(theta_inds, phi_inds)]
+
+    theta,phi = np.meshgrid(theta, phi)
+
+    # sphere
+    x = R * np.sin(theta) * np.cos(phi)
+    y = R * np.sin(theta) * np.sin(phi)
+    z = R * np.cos(theta)
+
+    x += position[0]
+    y += position[1]
+    z += position[2]
+
+    # create 3d Axes
+    plot.plot_surface(x.T, y.T, z.T, facecolors=img, cstride=1, rstride=1, shade=True, edgecolor='none', alpha=1) # we've already pruned ourselves
+
+
 max_orbit = 0
 for planet in planets:
-    d = np.linalg.norm(planet.position) * 1.5
+    d = np.linalg.norm(planet.position)
     if d > max_orbit:
-        max_orbit = d
+        max_orbit = d*1.5
 # max_orbit = np.max(neptune.position) * 1.5
 
 max_t = 40000000000
@@ -73,16 +104,20 @@ for i in range(0, int(max_t/h)):
 
 
 
-fig = plt.figure()
+fig = plt.figure(figsize=[8, 8])
 ax = fig.add_subplot(111, projection='3d')
+ax.set_box_aspect([1,1,1])
+ax.set_xlim(-max_orbit, max_orbit)
+ax.set_ylim(-max_orbit, max_orbit)
+ax.set_zlim(-max_orbit, max_orbit)
+ax.grid(False)
+plt.title('Solar System')
+
+
 
 plt.figure(figsize=[6, 6])
 for planet in planets:
     transpose = np.transpose(planet.path)
-    ax.plot(transpose[0], transpose[1], transpose[2])
+    #ax.plot(transpose[0], transpose[1], transpose[2])
+    place_planet(planet.radius * 5000000, planet.texture, ax, planet.position, 50)
 
-
-# plt.show()
-
-# plt.plot(np.transpose(sun.path)[0])
-# plt.show()
---
+++ Python/plot_sphere.py
@@ -0,0 +1,201 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from itertools import product, combinations
+from matplotlib.colors import LightSource
+from matplotlib.image import imread
+from tvtk.api import tvtk
+
+# %%
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.set_aspect("equal")
+
+# draw sphere
+u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+x = np.cos(u)*np.sin(v)
+y = np.sin(u)*np.sin(v)
+z = np.cos(v)
+ax.plot_wireframe(x, y, z, color="r")
+
+plt.show()
+
+
+# Create a meshgrid for the spherical coordinates
+phi = np.linspace(0, np.pi, 20)  # Azimuthal angle
+theta = np.linspace(0, 2 * np.pi, 40)  # Polar angle
+phi, theta = np.meshgrid(phi, theta)
+
+# Convert spherical coordinates to Cartesian coordinates
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the mesh sphere
+ax.plot_surface(x, y, z, color='b', alpha=0.5)
+
+# Set the aspect ratio of the plot to be equal
+ax.set_box_aspect([1,1,1])
+
+# Set labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Show the plot
+plt.show()
+
+# %%
+
+earth_texture = imread('Earth_4k.png')
+
+# Create a meshgrid for the spherical coordinates
+phi = np.linspace(0, np.pi, 20)  # Azimuthal angle
+theta = np.linspace(0, 2 * np.pi, 40)  # Polar angle
+phi, theta = np.meshgrid(phi, theta)
+
+# Convert spherical coordinates to Cartesian coordinates
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Create a light source
+ls = LightSource(azdeg=0, altdeg=45)
+
+# Plot the mesh sphere with lighting
+ax.plot_surface(x, y, z, rstride=1, cstride=1, color='b', alpha=1, facecolors=earth_texture)
+# ax.plot_surface(x, y, z, color='b', alpha=0.5)
+
+# Set the aspect ratio of the plot to be equal
+ax.set_box_aspect([1,1,1])
+
+# Set labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Show the plot
+plt.show()
+
+# %%
+
+# Create a meshgrid for the spherical coordinates
+phi = np.linspace(0, np.pi, 20)  # Azimuthal angle
+theta = np.linspace(0, 2 * np.pi, 40)  # Polar angle
+phi, theta = np.meshgrid(phi, theta)
+
+# Convert spherical coordinates to Cartesian coordinates
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+
+# Load the texture image
+earth_texture = imread('Earth_4k.png')  # Replace 'texture.png' with the path to your PNG image
+plt.imshow(earth_texture)
+
+# %%
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the mesh sphere with texture
+ax.plot_surface(x, y, z, rstride=1, cstride=1, facecolors=earth_texture, alpha=0.5, 
+                shade=False)
+
+# Set the aspect ratio of the plot to be equal
+ax.set_box_aspect([1, 1, 1])
+
+# Set labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Show the plot
+plt.show()
+
+
+# %%
+image = imread('Earth_4k.png')
+
+plt.imshow(image)
+
+# %%
+img = plt.imread('Jupiter_2k.png')
+
+# define a grid matching the map size, subsample along with pixels
+theta = np.linspace(0, np.pi, img.shape[0])
+phi = np.linspace(0, 2*np.pi, img.shape[1])
+
+count = 20 # keep 180 points along theta and phi
+theta_inds = np.linspace(0, img.shape[0] - 1, count).round().astype(int)
+phi_inds = np.linspace(0, img.shape[1] - 1, count).round().astype(int)
+theta = theta[theta_inds]
+phi = phi[phi_inds]
+img = img[np.ix_(theta_inds, phi_inds)]
+
+theta,phi = np.meshgrid(theta, phi)
+R = 2
+
+# sphere
+x = R * np.sin(theta) * np.cos(phi)
+y = R * np.sin(theta) * np.sin(phi)
+z = R * np.cos(theta)
+
+z += 1
+
+
+# %%
+# create 3d Axes
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x.T, y.T, z.T, facecolors=img, cstride=1, rstride=1, shade=True, edgecolor='none', alpha=1) # we've already pruned ourselves
+
+# make the plot more spherical
+ax.axis('scaled')
+
+# %%
+def place_planet(R, image, position=[0,0,0], count=20):
+    img = plt.imread(image)
+
+    # define a grid matching the map size, subsample along with pixels
+    theta = np.linspace(0, np.pi, img.shape[0])
+    phi = np.linspace(0, 2*np.pi, img.shape[1])
+
+    theta_inds = np.linspace(0, img.shape[0] - 1, count).round().astype(int)
+    phi_inds = np.linspace(0, img.shape[1] - 1, count).round().astype(int)
+    theta = theta[theta_inds]
+    phi = phi[phi_inds]
+    img = img[np.ix_(theta_inds, phi_inds)]
+
+    theta,phi = np.meshgrid(theta, phi)
+    R = 1
+
+    # sphere
+    x = R * np.sin(theta) * np.cos(phi)
+    y = R * np.sin(theta) * np.sin(phi)
+    z = R * np.cos(theta)
+
+    x += position[0]
+    y += position[1]
+    z += position[2]
+
+    # create 3d Axes
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x.T, y.T, z.T, facecolors=img, cstride=1, rstride=1, shade=True, edgecolor='none', alpha=1) # we've already pruned ourselves
+
+    # make the plot more spherical
+    ax.axis('scaled')
+
+# %%
+place_planet(0.5, 'Jupiter_2k.png', [0, 5, 5])
