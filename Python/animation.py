64b70ed1ce39cdd2b84d23e0bc2d66310f7db0db
AysenJahola <aysenjahola3@gmail.com>
2024-05-25 01:03:29 +0100

    Full animation done

--- Python/animation.py
+++ Python/animation.py
@@ -5,6 +5,8 @@ from planet import place_planet
 from matplotlib.colors import LightSource
 
 def animate(storage, planets):
+
+    frame_size = (1920, 1080)  # Width and height of the video
     # Read background image
     background_image = cv2.imread('pexels-francesco-ungaro-998641.jpg')
 
@@ -14,18 +16,18 @@ def animate(storage, planets):
 
     # OpenCV VideoWriter
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec
-    fps = 60  # Frames per second
-    frame_size = (1920, 1080)  # Width and height of the video
-    out = cv2.VideoWriter('SolarSystem1.mp4', fourcc, fps, frame_size)
-    
-    # Define the initial camera angles
-    initial_elev = 10  # Initial elevation angle
-    final_elev = 35  # Initial elevation angle
-    initial_azim = 0   # Initial azimuth angle
-    final_azim = 160   # Final azimuth angle to complete a full rotation
+    fps = 120  # Frames per second
+
+    out = cv2.VideoWriter('SolarSystemM2.mp4', fourcc, fps, frame_size)
     num_frames = storage.shape[1]
-    elev_step = (final_elev - initial_elev) / num_frames  # Calculate the step size for azimuth angle
-    azim_step = (final_azim - initial_azim) / num_frames  # Calculate the step size for azimuth angle
+
+    # Define the initial camera angles
+    # initial_elev = 0  # Initial elevation angle
+    # final_elev = 30  # Initial elevation angle
+    # initial_azim = 0   # Initial azimuth angle
+    # final_azim = 160   # Final azimuth angle to complete a full rotation
+    # elev_step = (final_elev - initial_elev) / num_frames  # Calculate the step size for azimuth angle
+    # azim_step = (final_azim - initial_azim) / num_frames  # Calculate the step size for azimuth angle
 
     sun = planets[0]
     #Initial Value Problem
@@ -35,17 +37,9 @@ def animate(storage, planets):
        for i in range(0, num_frames):
             planet.path.append(storage[0+3*(j-1):3+3*(j-1), i])
     
-    neptune = planets[-1]
-    max_orbit = 0
     # Loop to create frames
     for N in range(num_frames):
         ax.clear()
-        # ax.plot(x[:i], y[:i], 'b')
-        # ax.set_xlim(-1.5, 1.5)
-        # ax.set_ylim(-1.5, 1.5)
-        # ax.set_title(f'Frame {i+1}/{len(theta)}')
-        i = 0
-        j = storage.shape[0]//2
         for planet in planets:
             if planet == sun:
                 continue
@@ -53,20 +47,8 @@ def animate(storage, planets):
             planet.position = planet.path[N]
 
         max_orbit = max(np.linalg.norm(planet.position) for planet in planets)
-        # max_orbit = 0 #initialising max orbit to 0
-        #This accounts for the maximum deviation from the centre of the planets orbits
-        # for planet in planets: #iterating through all of the planets and calculating the euclidian distance, if this passes the max, set as new max
-        #     d = np.linalg.norm(planet.position)
-        #     if d > max_orbit:
-        #         max_orbit = d
+        # max_orbit = 30
 
-    
-        # #Initial Value Problem
-        # for k, planet in enumerate(planets):
-        #     # for p in range(0, N+1):
-        #     planet.path.append(storage[0+3*k:3+3*k, N])  
-                # RungeKutta4_v1(planet, planets, h)
-                # planet.position = EulersMethod(planet, planets, h)
         ax.axis('off')
         ax.set_box_aspect([1,1,1])
         ax.set_xlim(-max_orbit, max_orbit)
@@ -80,30 +62,23 @@ def animate(storage, planets):
             path = np.array(planet.path)
             transpose = np.transpose(path[:N])
             ax.plot(*transpose)
-        # for planet in planets:
-        #     if len(planet.path) > 0:
-        #         transpose = np.transpose(np.array(planet.path))
-        #         ax.plot(transpose[0], transpose[1], transpose[2])
-        #         place_planet(planet.radius, planet.texture, ax, planet.position, 60)
         
-        # Update the camera view angle
-        current_azim = initial_azim + N * azim_step
-        current_elev = initial_elev + N * elev_step
-        ax.view_init(elev=current_elev, azim=current_azim)
+        # # Update the camera view angle
+        # current_azim = initial_azim + N * azim_step
+        # current_elev = initial_elev + N * elev_step
+        # ax.view_init(elev=current_elev, azim=current_azim)
 
         # Save the plot as a PNG with a transparent background
-        fig.savefig('temp_frame.png', transparent=True)
+        fig.savefig('temp_frame1.png', transparent=True)
 
         # Read the saved PNG file
-        frame_with_transparency = cv2.imread('temp_frame.png', cv2.IMREAD_UNCHANGED)
-        # frame_with_transparency = cv2.resize(frame_with_transparency, frame_size)
-
+        frame_with_transparency = cv2.imread('temp_frame1.png', cv2.IMREAD_UNCHANGED)
+        frame_with_transparency = cv2.resize(frame_with_transparency, frame_size)
+        
+        resized_background = cv2.resize(background_image, frame_size)
         # Split the image into BGR and Alpha channels
         bgr = frame_with_transparency[:, :, :3]
         alpha = frame_with_transparency[:, :, 3]
-
-        # Resize background image to match the size of the frame
-        resized_background = cv2.resize(background_image, frame_size)
 
         # Create an alpha mask
         alpha_mask = alpha / 255.0
@@ -117,32 +92,9 @@ def animate(storage, planets):
         # Write the frame to the video
         out.write(resized_background)
 
-
-        # Draw the canvas and convert to image
-        # fig.canvas.draw()
-        # img = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        # img = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
-        # # Convert RGB to BGR for OpenCV
-        # img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-        # # Resize background image to match the size of the frame
-        # resized_background = cv2.resize(background_image, frame_size)
-
-        # # Overlay animation on background image
-        # combined_frame = resized_background.copy()
-        # combined_frame[:img_bgr.shape[0], :img_bgr.shape[1]] = img_bgr
-
-
-        # # Write frame to video
-        # out.write(combined_frame)
-        #     # Close the plot
-
-
     # Release the video writer
     out.release()
         
     plt.close(fig)
-
 
     print("Video has been written successfully.")
--- Python/main.py
+++ Python/main.py
@@ -17,12 +17,11 @@ AU = 149597870700 # astronomical unit in meters
 #The following shows the creation of the planets. These values are intial conditions with values from the link above.
 v_mw = 0# * 60 * 60 # m/s
 scale = 3000000
-jupiter = Planet("Jupiter", 1.898e27, [2.657301572545939E+00, 4.244832633235739E+00, -7.706081756999565E-02], [-6.480998680299733E-03, 4.362422858591558E-03, 1.268648293569800E-04+v_mw], radius=scale*69911/AU, texture= os.path.join("textures", "Jupiter_2k.png"))
-# jupiter = Planet("Jupiter", 1.898e27, [2.660511872500166e+00, 4.246331340893854e+00, -7.683955467163552e-02], [-5.923603532402550e-03, 3.369365015933616e-03, 2.621050098384081e-05], radius=10000000*69911/AU, texture= os.path.join("textures", "Jupiter_2k.png"))
-saturn  = Planet("Saturn", 5.683e+26, [9.211655736771199E+00, -2.998507001947348E+00, -3.146252679038145E-01], [1.415075980682737E-03, 5.293746502200081E-03, -1.483576235830070E-04+v_mw], radius=scale*58232/AU, texture=os.path.join("textures", "Saturn_2k.png"))
-sun     = Planet("Sun", 1.989e30, [-7.225998149491247E-03, -3.805650685824385E-03, 2.018536538194618E-04], [5.902551462508057E-06, -5.950748571015161E-06, -7.997101158836507E-08+v_mw], radius=scale/7*6.9634e5/AU, texture=os.path.join("textures", "Venus_2k.png")) # the sun here is assumed to start at zero postion
-uranus  = Planet("Uranus", 8.681e25, [1.183100464891022E+01, 1.560581328660078E+01, -9.531293561809405E-02], [-3.163272718481608E-03, 2.192851241912579E-03, 4.915878565000892E-05+v_mw], radius=scale*25362/AU, texture=os.path.join("textures", "Uranus_2k.png"))
-neptune = Planet("Neptune", 1.024e+26, [2.985499980827878E+01, -1.358647720327443E+00 , -6.600608487568795E-01], [1.220565817760229E-04, 3.154546893302934E-03, -6.780559909533712E-05+v_mw], radius=scale*24624/AU, texture=os.path.join("textures", "Neptune_2k.png"))
+jupiter = Planet("Jupiter", 1.898e27, [2.657301572545939E+00, 4.244832633235739E+00, -7.706081756999565E-02], [-6.480998680299733E-03, 4.362422858591558E-03, 1.268648293569800E-04], radius=scale*69911/AU, texture= os.path.join("textures", "Jupiter_2k.png"))
+saturn  = Planet("Saturn", 5.683e+26, [9.211655736771199E+00, -2.998507001947348E+00, -3.146252679038145E-01], [1.415075980682737E-03, 5.293746502200081E-03, -1.483576235830070E-04], radius=scale*58232/AU, texture=os.path.join("textures", "Saturn_2k.png"))
+sun     = Planet("Sun", 1.989e30, [-7.225998149491247E-03, -3.805650685824385E-03, 2.018536538194618E-04], [5.902551462508057E-06, -5.950748571015161E-06, -7.997101158836507E-08], radius=scale/7*6.9634e5/AU, texture=os.path.join("textures", "Sun_2k.png")) # the sun here is assumed to start at zero postion
+uranus  = Planet("Uranus", 8.681e25, [1.183100464891022E+01, 1.560581328660078E+01, -9.531293561809405E-02], [-3.163272718481608E-03, 2.192851241912579E-03, 4.915878565000892E-05], radius=scale*2*25362/AU, texture=os.path.join("textures", "Uranus_2k.png"))
+neptune = Planet("Neptune", 1.024e+26, [2.985499980827878E+01, -1.358647720327443E+00 , -6.600608487568795E-01], [1.220565817760229E-04, 3.154546893302934E-03, -6.780559909533712E-05], radius=scale*2*24624/AU, texture=os.path.join("textures", "Neptune_2k.png"))
 
 # Storing the created planets into a list named planets
 planets = [
@@ -37,7 +36,7 @@ mass = [sun.mass, jupiter.mass, saturn.mass, uranus.mass, neptune.mass]
 distance = [sun.position, jupiter.position, saturn.position, uranus.position, neptune.position]
 velocity = [jupiter.velocity, saturn.velocity, uranus.velocity, neptune.velocity]
 
-N = 1000
+N = 2000
 distance = np.concatenate(distance).tolist()
 velocity = np.concatenate(velocity).tolist()
 
@@ -51,8 +50,8 @@ vector = np.concatenate((distance[3:], velocity)) #not including the sun mass
 Storage_R = RungeKutta4_v1(vector, 0.5, N, C_val)
 
 #Applying Adam-Bashfourth Moulton
-initial4 =  RungeKutta4_v1(vector, 200, 4, C_val) #Initial 4 using Runge Kutta to put into ABM
-Storage = ABM_4(initial4, 200, N, C_val)
+initial4 =  RungeKutta4_v1(vector, 0.25, 4, C_val) #Initial 4 using Runge Kutta to put into ABM
+Storage = ABM_4(initial4, 100, N, C_val)
 
 animate(Storage, planets)
 # i = 0
